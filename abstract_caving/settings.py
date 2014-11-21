b49f2bae2a0bc3da599aee53e262e231cb8b1317
dlb8685 <dlb8685@gmail.com>
2014-11-21 16:26:51 -0600

    November 21, 2014 -- trivial tweak

--- abstract_caving/settings.py
+++ abstract_caving/settings.py
@@ -1,9 +1,7 @@
 """
 Django settings for abstract_caving project.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/topics/settings/
-
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
